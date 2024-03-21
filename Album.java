2febf20cf0adb5724acc8c5e4b0c926eea444a80
Dougley <dougley.doll@gmail.com>
2024-03-20 22:07:57 -0300

    Commit alinhamento classe Album

---
+++ Album.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Album {
+    private String nome;
+    private String ano_lancamento;
+    private ArrayList<Musica> musicas = new ArrayList<Musica>();
+
+
+    public Album(String nome, String ano_lancamento) {
+        this.nome = nome;
+        this.ano_lancamento = ano_lancamento;
+
+
+    }
+
+    public void add_musica(Musica musica){
+
+        this.musicas.add(musica);
+
+    }
+}
