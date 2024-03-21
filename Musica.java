64d4d94cac83ec4219490e96061d8cae9e2a010e
Dougley <dougley.doll@gmail.com>
2024-03-20 22:12:49 -0300

    Implementação classe Musica

---
+++ Musica.java
@@ -0,0 +1,13 @@
+public class Musica {
+
+    private String titulo_musica;
+    private String artista;
+    private int duracao;
+
+    public Musica(String titulo_musica, String artista, int duracao){
+
+        this.titulo_musica = titulo_musica;
+        this.artista = artista;
+        this.duracao = duracao;
+    }
+}
